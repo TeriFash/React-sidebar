7d86a6e7bb05535f69640158107ba299a4651181
TeriFash <teri.fash@gmail.com>
2019-03-10 15:42:06 +0200

    Side menu separated menu

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+
 import SideMenu from './components/SideMenu';
 import MenuList from './data/MenuItems';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
 import Error from './pages/Error';
+
 import './style/App.scss';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.toggleClass = this.toggleClass.bind(this);
-        this.state = {
-            active: false,
-        };
-    }
-
-    toggleClass() {
-        const currentState = this.state.active;
-        this.setState({ active: !currentState });
-    };
-
     render() {
         return (
-            <div className="App">
-                <BrowserRouter>
-                    <div>
-                        <nav className={`Slide-menu ${this.state.active ? 'is-active': null}`}>
-                            <SideMenu list={MenuList}/>
-                            <button onClick={this.toggleClass} className={`Menu-btn ${this.state.active ? 'is-active': null}`} type="button" aria-label="Menu" aria-controls="navigation">
-                            <span className="Menu-btn__block">
-                                <span className="Menu-btn__inner"></span>
-                            </span>
-                            </button>
-                        </nav>
-                        <Switch>
-                            <Route path="/" component={Home} exact />
-                            <Route path="/about" component={About} />
-                            <Route path="/services" component={Services} />
-                            <Route path="/projects" component={Projects} />
-                            <Route path="/contacts" component={Contacts} />
-                            <Route component={Error} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <nav>
+                        <SideMenu list={MenuList}/>
+                    </nav>
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                        <Route path="/about" component={About} />
+                        <Route path="/services" component={Services} />
+                        <Route path="/projects" component={Projects} />
+                        <Route path="/contacts" component={Contacts} />
+                        <Route component={Error} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
-
 
 export default App;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
 
 class List extends Component {
     constructor(props) {
         super(props);
-
+        this.toggleClass = this.toggleClass.bind(this);
         this.state = {
             list: props.list,
+            active: false,
         };
     }
+
+    toggleClass() {
+        const currentState = this.state.active;
+        this.setState({active: !currentState});
+    };
 
     render() {
 
         let block = (
-            <ul>
-                {this.state.list.map((item) =>
-                    <li
-                        key={item.name.toString()}
-                    >
-                        <NavLink to={item.path}>{item.name}</NavLink>
+            <div className={`Slide-menu ${this.state.active ? 'is-active' : null}`}>
+                <ul>
+                    {this.state.list.map((item) =>
+                        <li
+                            key={item.name.toString()}
+                        >
+                            <NavLink to={item.path}>{item.name}</NavLink>
 
-                    </li>
-                )}
-            </ul>
+                        </li>
+                    )}
+                </ul>
+                <MenuButton toggleClass={this.toggleClass}/>
+            </div>
         );
         return (
             block
@@ -30,5 +39,18 @@ class List extends Component {
     }
 }
 
+class MenuButton extends Component {
+    render() {
+        return (
+            <button onClick={this.props.toggleClass}
+                    className={`Menu-btn`}
+                    type="button">
+                        <span className="Menu-btn__block">
+                            <span className="Menu-btn__inner"></span>
+                        </span>
+            </button>
+        );
+    };
+}
 
 export default List;
